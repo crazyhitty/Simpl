770ee4703c278841d17f8b5d9c1565d17e7cc029
Kartik Sharma <cr42yh17m4n@gmail.com>
2019-02-17 20:55:40 +0530

    Fix bundle.sh to create bundle zip file without root folder

--- bundle.sh
+++ bundle.sh
@@ -2,17 +2,25 @@
 LIGHT_GREEN='\033[1;32m'
 NO_COLOR='\033[0m'
 
-# Current epoch timestamp
-BUILD_EPOCH_TIMESTAMP="$(date +'%s')"
-
 # Get Latest Commit ID
 COMMIT_HASH="$(git log -1 --format="%h" -n 1)"
 
-# Path and name of the zip bundle to be created
-RELEASE_ZIP_FILE="releases/simpl-release-bundle-$COMMIT_HASH-$BUILD_EPOCH_TIMESTAMP.zip"
+# Name of the zip bundle to be created
+RELEASE_ZIP_FILE="simpl-release-bundle-$COMMIT_HASH.zip"
 
+# Build the extension
 npm run build
-mkdir -p releases
-zip -r $RELEASE_ZIP_FILE extension/*
 
-echo "${LIGHT_GREEN}\nBundle created at path $RELEASE_ZIP_FILE"
+# Enter extension directory
+cd extension
+
+# Create zip bundle including all files available in this "extension" directory
+zip -r -FS $RELEASE_ZIP_FILE *
+
+# Create releases directory if it doesn't exist in main project directory
+mkdir -p ../releases
+
+# Move the created file to releases directory
+mv $RELEASE_ZIP_FILE ../releases
+
+echo "${LIGHT_GREEN}\nBundle created at path releases/$RELEASE_ZIP_FILE"
